ee006502d28afc2d54568ac5be8ba7a8d40e4615
beatriz <beatrizmcoimbra@outlook.com>
2016-11-22 12:12:36 -0200

    adicionando testes ao 1.42 e 1.43 e nova versão do 2.18

--- cap-1/solucao-1.43.rkt
+++ cap-1/solucao-1.43.rkt
@@ -1,9 +1,7 @@
 #lang racket
 
-<<<<<<< HEAD
+(require rackunit)
 
-=======
->>>>>>> f1e4f2a613e33d7654cf1ae4a03dbdaf07c10b07
 (define (compose f g)
   (lambda (x) (f (g x))))
 
@@ -11,8 +9,6 @@
   (cond ((< n 0) (error "'n' can't be a negative number"))
         ((= n 0) null)
         ((= n 1) f)
-<<<<<<< HEAD
         (else (compose f (repeated f (- n 1))))))
-=======
-        (else (compose f (repeated f (- n 1))))))
->>>>>>> f1e4f2a613e33d7654cf1ae4a03dbdaf07c10b07
+
+(check-equal? ((repeated sqr 2) 5) 625)
